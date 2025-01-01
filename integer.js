e518d01a5bf60d4c63e0f7c6f8ed5e70602dc4c6
CoderByTm <hero.uz1986@gmail.com>
2025-01-01 14:26:44 +0500

    integer.js

---
+++ integer.js
@@ -0,0 +1,111 @@
+// Integer11
+
+// let a = 2;
+// let b = 3;
+// let c = a + b;
+// let d = a * b;
+// console.log(c, d);
+
+// let a = 2;
+// let b = 1;
+// let c = a + b;
+// let d = a * b;
+// console.log(c, d);
+
+// let a = 8;
+// let b = 1;
+// let c = a + b;
+// let d = a * b;
+// console.log(c, d);
+
+// let a = 1;
+// let b = 0;
+// let c = a + b;
+// let d = a * b;
+// console.log(c, d);
+
+
+// Integer12
+
+// let n = 12;
+// let son = 2 * Math.pow((n + 3), 2)
+// console.log(son);
+
+
+// Integer 13
+
+// let n = 5;
+// let son = Math.pow(((3 + n)/2), 2)
+// console.log(son);
+
+
+// Integer 14
+
+// let n = 4;
+// let x = 2;
+// let a = Math.pow((n), x);
+// let b = Math.pow((6), x);
+// let y = a + b;
+// console.log(y);
+
+
+// Integer 15
+
+// let a = -8;
+// let son = Math.abs(a);
+// console.log(son);
+
+// let a = -56;
+// let son = Math.abs(a);
+// console.log(son);
+
+// let a = -45;
+// let son = Math.abs(a);
+// console.log(son);
+
+
+// Integer 16
+
+// let m = 3.456;
+// let a = Math.round(m*100) /100;
+// console.log(m.toFixed(2));
+
+// let m = 5.678;
+// let a = Math.round(m*100) /100;
+// console.log(m.toFixed(2));
+
+// let m = 7.5;
+// let a = Math.round(m*100) /100;
+// console.log(m.toFixed(1));
+
+
+// Integer 17
+
+// x4+5x2+x3y
+// let x = 2;
+// let y = 3;
+// let a = Math.pow(x, 4) + 5 * Math.pow(x, 2) + Math.pow(x, 3) * y;
+// console.log(a);
+
+
+// Integer 18
+
+// let a = 230;
+// let x = Math.floor(a / 100);
+// let y = Math.floor((a % 100) /10);
+// let z = a % 10;
+
+// let qushish = x + y + z;
+// let kupaytirish = x * y * z;
+
+// console.log(qushish, kupaytirish);
+
+let a = 241;
+let x = Math.floor(a / 100);
+let y = Math.floor((a % 100) /10);
+let z = a % 10;
+
+let qushish = x + y + z;
+let kupaytirish = x * y * z;
+
+console.log(qushish, kupaytirish);
--- main.js
+++
@@ -1,5 +0,0 @@
-let a = 2;
-let b = 3;
-let c = a + b;
-let d = a * b;
-console.log(c, d);
