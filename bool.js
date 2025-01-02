b831076a3cfabab2fc7b32af74b4a75191d014c2
CoderByTm <hero.uz1986@gmail.com>
2025-01-02 20:23:08 +0500

    bool.js

---
+++ bool.js
@@ -0,0 +1,141 @@
+// bool 1
+
+// let x = 5 == 5;
+// console.log(x);
+
+// let x = 3 == 4;
+// console.log(x);
+
+
+// bool 2
+
+// let a = 7
+// let x = a == 7;
+// console.log(x);
+
+// let a = 9
+// let x = a == 7;
+// console.log(x);
+
+// bool 3
+
+// let b = 3
+// let x = b > 0;
+// console.log(x);
+
+// let b = -6
+// let x = b > 0;
+// console.log(x);
+
+// bool 4
+
+// let a = -6
+// let x = a < 0;
+// console.log(x);
+
+// let a = 3
+// let x = a < 0;
+// console.log(x);
+
+
+// bool 5
+
+// let a = 7;
+// let result = a % 2 !== 0;
+// console.log(result);
+
+// let a = 8;
+// let result = a % 2 !== 0;
+// console.log(result);
+
+
+// bool 6
+
+// let a = 7;
+// let result = a % 2 == 0;
+// console.log(result);
+
+// let a = 8;
+// let result = a % 2 == 0;
+// console.log(result);
+
+
+// bool 7
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+// let result = (a < b && b < c) || (c < b && b < a);
+// console.log(result);
+
+// let a = 8;
+// let b = 3;
+// let c = 9;
+// let result = (a < b && b < c) || (c < b && b < a);
+// console.log(result);
+
+
+// bool 8
+// let a = 11;
+// let b = 16;
+// let result = a > 0 && b > 0;
+// console.log(result);
+
+// let a = -5;
+// let b = 8;
+// let result = a > 0 && b > 0;
+// console.log(result);
+
+// let a = 9;
+// let b = -1;
+// let result = a > 0 && b > 0;
+// console.log(result);
+
+// let a = -4;
+// let b = -3;
+// let result = a > 0 && b > 0;
+// console.log(result);
+
+// bool 9
+
+// let a = -5;
+// let b = -9;
+// let result = a < 0 && b < 0;
+// console.log(result);
+
+// let a = -4;
+// let b = 8;
+// let result = a < 0 && b < 0;
+// console.log(result);
+
+// let a = 10;
+// let b = -2;
+// let result = a < 0 && b < 0;
+// console.log(result);
+
+// let a = 4;
+// let b = 3;
+// let result = a < 0 && b < 0;
+// console.log(result);
+
+// bool 10
+
+// let a = 4;
+// let b = 8;
+// let result = (a % 2 === 0) && (b % 2 === 0);
+// console.log(result);
+
+// let a = 3;
+// let b = 12;
+// let result = (a % 2 === 0) && (b % 2 === 0);
+// console.log(result);
+
+// let a = 10;
+// let b = 7;
+// let result = (a % 2 === 0) && (b % 2 === 0);
+// console.log(result);
+
+// let a = 4;
+// let b = 3;
+// let result = (a % 2 === 0) && (b % 2 === 0);
+// console.log(result);
